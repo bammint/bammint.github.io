c55b7fcfaac38d5fc74a9c6ea6f8d5481d37bcbc
bammint <baikmalss@naver.com>
2024-01-01 23:05:36 +0900

    0101

---
+++ MyPage/src/styles/GlobalStyle.js
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    --dark-bg: #262626;
+    --blue-bg: #464D5E;
+    --gray-1: #BCB4B4;
+    --deep-dark: #1E1E1E;
+    --blue-dark: #484B54;
+    --gray-2: #363636;
+    --white : white;
+    --black: black;
+    --black2: #3B3B3B;
+  }
+  html{
+    font-size: 10px;
+    font-family: 'Roboto Mono';
+    background-color: var(--blue-bg);
+  }
+  ul,li{
+    list-style: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  img, svg{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  button{
+    outline: none
+  }
+  .container {
+    max-width: 1200px;
+    width: 90%;
+    margin: 0 auto;
+  }
+/* Smooth Scroll  */
+  [data-scrollbar] {
+    height: 100vh;
+    overflow: hidden;
+    background-color: var(--gray-1);
+    .scroll-content {
+      background-color: var(--dark-bg);
+    }
+    .scrollbar-track.scrollbar-track-y {
+      background: var(--deep-dark);
+      .scrollbar-thumb-y {
+        background: var(--gray-1);
+      }
+    }
+  }
+`;
+export default GlobalStyles;
